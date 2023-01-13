b09f9166944b310c6ac248ac7e30a16679a34cb6
solesicklab14 <solesick@cmu.ac.th>
2023-01-13 16:32:49 +0700

    solesicklab14_1_completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,33 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+
+
+
+void stat(const double A[],int N,double B[])
+{
+    double sum=0, Arithmetic=0, SD=0, GM=1, HMF=0, MAX=0, MIN=100;
+    for(int i=0; i<N; i++){
+        sum += A[i];
+        SD += pow(A[i],2);
+        GM *= A[i];
+        HMF += 1.0/A[i];
+        if(MAX<A[i])
+        {
+            MAX = A[i];
+        }
+        if(MIN>A[i])
+        {
+            MIN = A[i];
+        }
+        
+    }
+    Arithmetic = sum/N;
+    B[0] = Arithmetic;
+    B[1] = sqrt((SD/N)-pow(Arithmetic,2));
+    B[2] = pow(GM,1.0/N);
+    B[3] = N/HMF; 
+    B[4] = MAX;
+    B[5] = MIN;
+}
